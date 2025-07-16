b1cda956c0ce1e64123aa32d937ca2c3cf27a1b9
Matthew Nguyen <matthewnguyen1230@gmail.com>
2025-07-16 15:04:02 +0900

    vercel fix 3

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
 // Inline IdentifyUser form for demo/testing
 function IdentifyUser() {
